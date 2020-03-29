9f826d8341a11b002f3d95fc5b2c3439c3707544
Yaroslav Harmatyuk <yarharm@gmail.com>
2020-03-29 02:19:48 -0400

    Add prettier and eslint
    Resolve linting errors and warnings

--- twitter-replica-backend/src/routes/api/posts.js
+++ twitter-replica-backend/src/routes/api/posts.js
@@ -1,28 +1,29 @@
-const asyncHandler = require("express-async-handler");
-const router = require("express").Router();
-const mongoose = require("mongoose");
-const Post = mongoose.model("Post");
+const asyncHandler = require('express-async-handler');
+const router = require('express').Router();
+const mongoose = require('mongoose');
+
+const Post = mongoose.model('Post');
 
 router.get(
-  "/",
+  '/',
   asyncHandler(async (req, res) => {
     const posts = await Post.find();
     res
       .json({
-        message: "Posts fetched successfully",
-        posts: posts
+        message: 'Posts fetched successfully',
+        posts,
       })
       .send();
   })
 );
 
 router.post(
-  "/",
+  '/',
   asyncHandler(async (req, res) => {
     const post = new Post({
       title: req.body.title,
       description: req.body.description,
-      content: req.body.content
+      content: req.body.content,
     });
     const ret = await post.save();
     res.json(ret).send();
