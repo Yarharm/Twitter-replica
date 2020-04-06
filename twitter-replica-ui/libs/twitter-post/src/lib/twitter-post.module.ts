628a16db3d45e9e560dd50613f479498de48f5d9
Yaroslav Harmatyuk <yarharm@gmail.com>
2020-04-06 00:31:32 -0400

    Add frontend routing for posts

--- twitter-replica-ui/libs/twitter-post/src/lib/twitter-post.module.ts
+++ twitter-replica-ui/libs/twitter-post/src/lib/twitter-post.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { RouterModule, Route } from '@angular/router';
 import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatCardModule } from '@angular/material/card';
@@ -9,15 +10,27 @@ import { MatExpansionModule } from '@angular/material/expansion';
 import { TwitterPostCreateComponent } from './post-create/post-create.component';
 import { TwitterPostListComponent } from './post-list/post-list.component';
 
+export const TwitterPostRoutes: Route[] = [
+  {
+    path: '',
+    component: TwitterPostListComponent,
+  },
+  {
+    path: 'create',
+    component: TwitterPostCreateComponent,
+  },
+];
+
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
+    RouterModule,
     MatInputModule,
     MatFormFieldModule,
     MatCardModule,
     MatButtonModule,
-    MatExpansionModule
+    MatExpansionModule,
   ],
   declarations: [TwitterPostCreateComponent, TwitterPostListComponent],
   exports: [
@@ -28,7 +41,7 @@ import { TwitterPostListComponent } from './post-list/post-list.component';
     MatFormFieldModule,
     MatCardModule,
     MatButtonModule,
-    MatExpansionModule
-  ]
+    MatExpansionModule,
+  ],
 })
 export class TwitterPostModule {}
