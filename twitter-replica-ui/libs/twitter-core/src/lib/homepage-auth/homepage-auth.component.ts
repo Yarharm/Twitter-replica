00c48f200e4ed038e7deea6dee8168f0d3ac4bd2
Yaroslav Harmatyuk <yarharm@gmail.com>
2020-05-28 01:06:14 -0400

    Add homepage component

---
+++ twitter-replica-ui/libs/twitter-core/src/lib/homepage-auth/homepage-auth.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'twitter-homepage-auth',
+  templateUrl: './homepage-auth.component.html',
+  styleUrls: ['./homepage-auth.component.scss'],
+})
+export class HomepageAuthComponent {}
--- twitter-replica-ui/libs/twitter-core/src/lib/twitter-core.module.ts
+++ twitter-replica-ui/libs/twitter-core/src/lib/twitter-core.module.ts
@@ -1,18 +1,37 @@
 import { NgModule } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { RouterModule, Route } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
+import { FlexLayoutModule } from '@angular/flex-layout';
 import { MaterialModule } from '../../../material.module';
 import { httpInterceptorProviders } from './http-interceptors';
 import { AuthGuard } from './http-guards';
 import { HeaderComponent } from './header/header.component';
 import { TwitterDialogErrorComponent } from './dialog-error/dialog-error.component';
+import { HomepageAuthComponent } from './homepage-auth/homepage-auth.component';
+
+export const TwitterCoreRoutes: Route[] = [
+  {
+    path: '',
+    component: HomepageAuthComponent,
+  },
+];
 
 @NgModule({
-  declarations: [HeaderComponent, TwitterDialogErrorComponent],
-  imports: [CommonModule, RouterModule, MaterialModule, HttpClientModule],
+  declarations: [
+    HeaderComponent,
+    TwitterDialogErrorComponent,
+    HomepageAuthComponent,
+  ],
+  imports: [
+    CommonModule,
+    RouterModule,
+    MaterialModule,
+    HttpClientModule,
+    FlexLayoutModule,
+  ],
   providers: [httpInterceptorProviders, AuthGuard],
   entryComponents: [TwitterDialogErrorComponent],
-  exports: [HeaderComponent],
+  exports: [HeaderComponent, HomepageAuthComponent],
 })
 export class TwitterCoreModule {}
